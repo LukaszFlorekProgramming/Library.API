9c19486c63a9413200e2aed1b00614a9b04e1bc7
Lukasz Florek <35149@studentwsei.pl>
2023-03-23 18:25:38 +0100

    created LibraryAPIController and model Library

---
+++ LibraryAPI/Controllers/LibraryAPIColtroller.cs
@@ -0,0 +1,20 @@
+﻿using LibraryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/LibraryAPI")]
+    [ApiController]
+    public class LibraryAPIColtroller : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Library> GetLibraries()
+        {
+            return new List<Library>
+            {
+                new Library{Id=1,Name="Czerwień Rubinu"},
+                new Library{Id=2,Name="Cień wiatru"}
+            };
+        }
+    }
+}
