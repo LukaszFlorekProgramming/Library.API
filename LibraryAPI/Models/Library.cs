52fa7a497df3d923c5126fee173b5983440c881c
Lukasz Florek <35149@studentwsei.pl>
2023-03-24 08:46:37 +0100

    created ApplicationDbContext

--- LibraryAPI/Models/Library.cs
+++ LibraryAPI/Models/Library.cs
@@ -1,7 +1,13 @@
-﻿namespace LibraryAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryAPI.Models
 {
     public class Library
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
