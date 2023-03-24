261863962eec5193046053c3223e2275dc67fa53
Lukasz Florek <35149@studentwsei.pl>
2023-03-24 08:53:46 +0100

    Add services AddDbContext

--- LibraryAPI/Data/ApplicationDbContext.cs
+++ LibraryAPI/Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace LibraryAPI.Data
+﻿using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Library> Libraries { get; set; }
     }
 }
